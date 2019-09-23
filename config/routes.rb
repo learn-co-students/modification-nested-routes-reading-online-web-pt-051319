79784edb3ba8fefbd9ce91db678ee55e8cf3eed6
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-09-23 13:52:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do
+    # This gives us access to /authors/:id/posts/new, and a new_author_post_path helper.
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
 end
